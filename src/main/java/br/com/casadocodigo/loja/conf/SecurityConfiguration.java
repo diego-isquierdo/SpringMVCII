4a3f1a92834e6ce3674822d83a5d606709f5f8f8
Diego Isquierdo <diego.isquierdo@hotmail.com>
2020-03-19 10:45:59 -0300

    Config de acesso usr em SecurityConfiguration

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -7,10 +9,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 /** Classe com as configurações de segurança
  * 
+ * habilita filtro de login
+ * 
  */
 
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-
+    //metodo de configuração
+    //configura quem pode acesar o quê
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/resources/**").permitAll()
+        .antMatchers("/carrinho/**").permitAll()
+        .antMatchers("/pagamento/**").permitAll()
+        .antMatchers("/produtos/form").hasRole("ADMIN")
+        .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+        .antMatchers(HttpMethod.GET, "/produtos").hasRole("ADMIN")
+        .antMatchers("/produtos/**").permitAll()
+        .antMatchers("/").permitAll()
+        .anyRequest().authenticated()
+        .and().formLogin();
+    }
 }
